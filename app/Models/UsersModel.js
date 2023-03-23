e735e84049fdd76ef782d08df3dec8f4d4c16ff3
MasDahni1337 <ekodahni@gmail.com>
2023-03-23 22:27:14 +0700

    update models

--- app/Models/UsersModel.js
+++ app/Models/UsersModel.js
@@ -24,12 +24,12 @@ class UsersModel extends Model{
     }
 
    update(id, data) {
-        this.service.where('id', id);
+        this.where('id', id);
         return this.update(data);
     }
 
    delete(id) {
-        this.service.where('id', id);
+        this.where('id', id);
         return this.delete();
     }
 
